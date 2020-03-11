976fc6bed68624e68d2c0d58cd11b7938aebb356
Jason Wong <tsanghan@gmail.com>
2020-03-11 20:34:51 +0800

    Inserted comments

--- runme.sh
+++ runme.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+###
+### Enable Byobu
+###
 byobu enable
+
+###
+### APT update, upgrade and install (auto)
+###
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y --no-install-recommends \
@@ -7,25 +14,46 @@ sudo apt install -y --no-install-recommends \
   libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
   xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
+###
+### Clone github repo pyenv
+###
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
+###
+### Export environment variable for proper execution of pyenv
+###
 export PATH="/home/ubuntu/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 
+###
+### Clone github repo pyenv-update
+###
 git clone https://github.com/pyenv/pyenv-update.git $(pyenv root)/plugins/pyenv-update
 
+###
+### Install python 3.8.1, set to global and rehssh
+###
 pyenv install 3.8.1
 pyenv global 3.8.1
 pyenv rehash
 pip install --upgrade pip
 pip install pipenv
 
+###
+### Make projects/ansible
+###
 mkdir -p projects/ansible
 cd projects/ansible
 
+###
+### Install in venv ansible and jemspath
+###
 pipenv install ansible
 pipenv install jmespath
 
+###
+### Update ~/.bashrc file so pyenv envs persist
+###
 cat <<EOF >> ~/.bashrc
 ###
 ### Pyenv
