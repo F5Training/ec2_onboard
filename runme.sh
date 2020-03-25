5dabd6fa4974fdd62ad63a31e5ffd7a7e817ae42
Jason Wong <tsanghan@gmail.com>
2020-03-25 21:00:13 +0800

    added python in apt install list

--- runme.sh
+++ runme.sh
@@ -27,7 +27,8 @@ sudo snap install lxd
 sudo apt install -y --no-install-recommends \
   make build-essential libssl-dev zlib1g-dev libbz2-dev \
   libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
-  xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+  xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
+  python
 
 ###
 ### Clone github repo pyenv
